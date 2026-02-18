483f4eda4f8da98026eb50fe056fce07c004868e
narendra4041 <narendra141994@gmail.com>
2026-02-18 13:07:00 +0100

    Add files via upload

---
+++ vector_rag.py
@@ -0,0 +1,45 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+from neo4j import GraphDatabase
+from neo4j_graphrag.embeddings.openai import OpenAIEmbeddings
+from neo4j_graphrag.retrievers import VectorRetriever
+from neo4j_graphrag.llm import OpenAILLM
+from neo4j_graphrag.generation import GraphRAG
+# Connect to Neo4j database
+driver = GraphDatabase.driver(
+    os.getenv("NEO4J_URI"), 
+    auth=(
+        os.getenv("NEO4J_USERNAME"), 
+        os.getenv("NEO4J_PASSWORD")
+    )
+)
+
+# Create embedder
+embedder = OpenAIEmbeddings(model="text-embedding-ada-002")
+
+# Create retriever
+retriever = VectorRetriever(
+    driver,
+    neo4j_database=os.getenv("NEO4J_DATABASE"),
+    index_name="moviePlots",
+    embedder=embedder,
+    return_properties=["title", "plot"],
+)
+
+# Create the LLM
+llm = OpenAILLM(model_name="gpt-4o", model_params={"temperature": 1})
+
+# Create GraphRAG pipeline
+rag = GraphRAG(retriever=retriever, llm=llm)
+
+# Search 
+query_text = "Find me movies about toys coming alive"
+
+result = rag.search(query_text=query_text, retriever_config={"top_k": 5}, return_context=True)
+
+print(result.answer)
+print("CONTEXT:", result.retriever_result.items)
+# CLose the database connection
+driver.close()
